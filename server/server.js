f8e6c6c18364ecacf9237b966c452cc8f32d46bf
Kyle Belfort <belfortk@gmail.com>
2019-02-28 02:16:01 -0800

    working on server

--- server/server.js
+++ server/server.js
@@ -5,15 +5,15 @@ const path = require('path');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('build'));
+app.use(express.static('public'));
 
 app.get('/ping',  (req, res) => res.send('pong'));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-// app.use(express.static('build'));
-// app.use(express.static('public'));
+// app.get('/', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 module.exports = app;
