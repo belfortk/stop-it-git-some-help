1cfae3b46a4e1be6d030db4945266dd13384958e
Kyle Belfort <belfortk@gmail.com>
2019-02-28 02:21:50 -0800

    just send everything pls

--- server/server.js
+++ server/server.js
@@ -5,15 +5,18 @@ const path = require('path');
 const app = express();
 
 app.use(morgan('dev'));
-// app.use(express.static(__dirname));
-// app.use(express.static(path.join(__dirname, 'build')));
-app.use(express.static('build'));
-app.use(express.static('public'));
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static('build'));
+// app.use(express.static('public'));
 
 app.get('/ping',  (req, res) => res.send('pong'));
 
-// app.get('/', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/*',  (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// app.use(express.static(__dirname));
+// app.use(express.static(path.join(__dirname, 'build')));
 
 module.exports = app;
